57f9ddd681a66d39fdb984821858462bd726ea95
Barnabas Kecskes <barnabas@riverskies.com>
2017-09-06 08:58:12 +0100

    Ensure language files are being used

---
+++ resources/lang/hu/newsletter_subscription.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'subscribe' => 'Sikeresen feliratkozott hírlevelünkre a(z) :email email címmel',
+    'unsubscribe' => 'Sikeresen leiratkozott hírlevelünkről a(z) :email email címmel',
+];
---
+++ tests/Feature/MessagesCanBeLocalisedTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Support\Facades\Mail;
+use Riverskies\LaravelNewsletterSubscription\NewsletterSubscription;
+use Tests\TestCase;
+
+class MessagesCanBeLocalisedTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /** @test */
+    public function subscribe_message_is_localised()
+    {
+        Mail::fake();
+        app()->setLocale('hu');
+
+        $response = $this->post($this->config('subscribe_url'), ['email'=>'john@example.com']);
+
+        $response->assertSessionHas('flash', 'Sikeresen feliratkozott hírlevelünkre a(z) john@example.com email címmel');
+    }
+
+    /** @test */
+    public function unsubscribe_message_is_localised()
+    {
+        app()->setLocale('hu');
+        $subscription = factory(NewsletterSubscription::class)->create();
+
+        $response = $this->get($this->getUnsubscribeUrlFor($subscription->hash));
+
+        $response->assertSessionHas('flash', "Sikeresen leiratkozott hírlevelünkről a(z) {$subscription->email} email címmel");
+    }
+}
