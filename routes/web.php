7acdd0f908b31702d8c54dfe66105b46a80fbfca
Barnabas Kecskes <barnabas@riverskies.com>
2017-09-06 12:35:23 +0100

    Ensure routes are enclosed in the web group to allow session handling

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,8 @@
 <?php
 
-Route::post(config('newsletter_subscription.subscribe_url'), 'Riverskies\LaravelNewsletterSubscription\Http\Controllers\NewsletterSubscriptionController@store');
-Route::get(config('newsletter_subscription.unsubscribe_url'), 'Riverskies\LaravelNewsletterSubscription\Http\Controllers\NewsletterSubscriptionController@destroy');
+Route::group(['middleware' => 'web'], function() {
+    Route::post(config('newsletter_subscription.subscribe_url'),
+        'Riverskies\LaravelNewsletterSubscription\Http\Controllers\NewsletterSubscriptionController@store');
+    Route::get(config('newsletter_subscription.unsubscribe_url'),
+        'Riverskies\LaravelNewsletterSubscription\Http\Controllers\NewsletterSubscriptionController@destroy');
+});
